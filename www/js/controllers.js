cdcb6899cdab6949cda9880b2869dfd93226cc8c
Jacob <jakegibs617@gmail.com>
2016-04-30 11:03:30 -0400

    added placeholder camera image

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,22 +1,26 @@
 angular.module('app.controllers', [])
-  
+
 .controller('signInCtrl', function($scope) {
 
 })
-   
+
 .controller('cameraCtrl', function($scope) {
 
+  $scope.pictureUrl = 'http://placehold.it/300x300';
+  $scope.takePicture = function() {
+
+  };
+
 })
-   
+
 .controller('contactClassCtrl', function($scope) {
 
 })
-   
+
 .controller('galleryCtrl', function($scope) {
 
 })
-   
+
 .controller('settingsCtrl', function($scope) {
 
 })
-       
