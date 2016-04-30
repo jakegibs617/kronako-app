6078ec0f634b3f631ea010176ab494a627b00386
Jacob <jakegibs617@gmail.com>
2016-04-30 11:11:10 -0400

    photos need to be tested on iphone at this point

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,11 +4,23 @@ angular.module('app.controllers', [])
 
 })
 
-.controller('cameraCtrl', function($scope) {
+.controller('cameraCtrl', function($scope, $cordovaCamera) {
 
   $scope.pictureUrl = 'http://placehold.it/300x300';
   $scope.takePicture = function() {
 
+    var options = {
+         destinationType: Camera.DestinationType.DATA_URL,
+         encodingType: Camera.EncodingType.JPEG
+       };
+
+    $cordovaCamera.getPicture({})
+      .then(function(data) {
+          // console.log('camera data: ' + angular.toJson(data));
+          $scope.pictureUrl = "data:image/jpeg;base64," + data;
+      }, function(error) {
+        // console.log('error data: ' + angular.toJson(data));
+      })
   };
 
 })
